a79176c2cc3d79b11bd7344c9fe2d7b0d73ce50a
Lytay <lytaytouch@gmail.com>
2014-09-05 00:26:40 +0700

    Update menu navigator with WP-Header Menu and Bootstrap

--- header.php
+++ header.php
@@ -11,40 +11,48 @@
 
 <div class="container-fluid">
     <div class="row">
+        
         <div class="col-md-2">
-            <img  class="img-responsive" alt="Responsive image" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
-<center><h3><?php bloginfo('description'); ?></h3></center>
-
+            <a href="<?php bloginfo('url'); ?>">
+                <img  class="img-responsive img-rounded" alt="Responsive image" src="<?php header_image(); ?>" alt="" />
+                <span class="text-center"><p class="custom-margin"><?php bloginfo('description'); ?></p></span>
+            </a>
+            
         </div>
+
         <div class="col-md-10">
             <nav class="navbar navbar-default" role="navigation">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-    </div>
-        <?php
-            wp_nav_menu( array(
-                'menu'              => 'primary',
-                'theme_location'    => 'header-menu',
-                'depth'             => 2,
-                'container'         => 'div',
-                'container_class'   => 'collapse navbar-collapse',
-                'container_id'      => 'bs-example-navbar-collapse-1',
-                'menu_class'        => 'nav navbar-nav',
-                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                'walker'            => new wp_bootstrap_navwalker())
-            );
-        ?>
-    </div>
-</nav>      
-
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a href="<?php bloginfo('url'); ?>" class="navbar-brand"><i class="fa fa-home fa-lg"></i></a>
+                  <!-- <a href="<?php bloginfo('url'); ?>"><i class="fa fa-home fa-3x"></i></a> -->
+                </div>
+    
+                <div id="navbarCollapse" class="collapse navbar-collapse">
+                        <?php
+                            wp_nav_menu( array(
+                                'menu'              => 'primary',
+                                'theme_location'    => 'header-menu',
+                                'container_id'      => 'navbarCollapsea',
+                                'menu_class'        => 'nav navbar-nav',
+                                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                                'walker'            => new wp_bootstrap_navwalker())
+                            );
+                        ?>
+                        <form role="search" class="navbar-form navbar-right">
+                            <div class="form-group">
+                                <input type="text" placeholder="Search" class="form-control">
+                            </div>
+                         </form>
+                </div>
+            </nav>
         </div>
     </div>
+
 </div>
--- searchform.php
+++
@@ -1,1 +0,0 @@
-searchforms.php
